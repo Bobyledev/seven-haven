10ecd4566af84b0e643d1bf300ba12eed5b4a5a6
Bobyledev <tucci.matthieu@hotmail.fr>
2026-01-18 22:01:56 +0100

    test:(java_script)add class send data localstorage

--- script_js/class_formulaire.js
+++ script_js/class_formulaire.js
@@ -3,12 +3,31 @@
 
 class formulaire{
 
-    constructor(id){
-        this.id =id;
-        this.formulaire = document.getElementById(id);
+    constructor(id) {
+        this.id = id;
+        this.formulaireHTML = document.getElementById(id);
         this.objet_formulaire =new FormData();
         this.liste = new Array();
-    }
+    };
 
-    
+    // envoi des données dans le local storage
+
+envoieFormulaire() {
+        this.objet_formulaire = new FormData(this.formulaireHTML);
+
+
+        this.objet_formulaire.forEach((value, key) => {
+        if (value !=="") {
+            this.liste.push([key, value]);
+
+        };
+    });
+
+
+        let JSONListe = JSON.stringify(this.liste);
+        localStorage.setItem("test", JSONListe)
+        document.getElementById("form").reset();
+}
+
+
 }
